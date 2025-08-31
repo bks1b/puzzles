bd31e2d5d93acb50959fc991a87899f4d0b4d405
1s3k3b <1s3k3b@gmail.com>
2025-08-31 21:59:53 +0200

    Sudoku solver

---
+++ puzzles/project-euler/96.hs
@@ -0,0 +1,7 @@
+!include ../sudoku.hs
+
+readAll :: [String] -> [[String]]
+readAll [] = []
+readAll (x : xs) = (map (\x -> if x == '0' then "" else [x]) $ concat $ take 9 xs) : (readAll $ drop 9 xs)
+
+result = show . sum . map (\x -> read ((concat . take 3 . head . solve) x) :: Int) . readAll . lines
---
+++ puzzles/sudoku-util.hs
@@ -0,0 +1,22 @@
+!include ./util.hs
+
+values = map show [1..9]
+
+size = length values
+blockSize = intSqrt size
+indices = [0..size * size - 1]
+blockIndices = [0..size - 1]
+
+getPos :: Int -> (Int, Int)
+getPos i = (mod i size, div i size)
+
+getIdx :: (Int, Int) -> Int
+getIdx (x, y) = x + y * size
+
+getBlock :: Int -> (Int, Int)
+getBlock i = (f x, f y) where
+    (x, y) = getPos i
+    f c = blockSize * (div c blockSize)
+
+getBlockIdx :: Int -> Int
+getBlockIdx i = x + y * blockSize where (x, y) = getBlock i
